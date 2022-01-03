c9c770974844260949edce9e9f2bd572e8e25131
szx1202 <44124690+szx1202@users.noreply.github.com>
2022-01-03 21:15:26 +0100

    Add files via upload

--- TurnOut_ESP32_PE_2022_V4_01.ino
+++ TurnOut_ESP32_PE_2022_V4_01.ino
@@ -28,13 +28,13 @@ const int numItems=16; //number of servo's to manage
 char servopos[3]; //4 is the string leght for Bluethooth command (F12, R12,...)
 
 // block for IR module
-unsigned int busyPins[]= {0, 0, 0, 0, 32, 33};  // array for occupation signal where connected occupation modules
+unsigned int busyPins[]= {0, 0, 0, 0, 32, 33};  // array for occupation signal 
 unsigned int itrPins []= {0, 0, 0, 0, 16, 17}; //array for pin where to connect ifr chip data pin used for interrpt 
 unsigned int relayPins[]= {25, 26, 27, 13, 0,0};  // array for Line Halt-Go 
 
 int valBlock;     // variable to store result
 int idxIR=0;      // store which IR started interrupt
-int BTled=12; // Led for BT status
+//int BTled=12; // Led for BT status
 String btBlock; // String to be sent via BT with the Block info LHx/LLx  
 bool isFree = true ; // char to store the status after interrupt false=blocked true=free
 
@@ -51,12 +51,9 @@ void setup()
 {
   //Setup usb serial connection to computer
   Serial.begin(115200);
-//  pinMode(BTled, OUTPUT);
-//  digitalWrite(BTled, LOW); 
   
- SerialBT.begin("ESP32Track"); //Bluetooth device name
- Serial.println("The device started, now you can pair it with bluetooth!");
-  
+  SerialBT.begin("ESP32Track"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
   // Pin for block module set as output
   for (int i=0; i<6;i++){
     if (busyPins[i]!=0){
@@ -64,60 +61,51 @@ void setup()
       digitalWrite(busyPins[i], LOW); //signal leds block OFF
     }
   }
-   for (int i=0; i<6;i++){
+  for (int i=0; i<6;i++){
     if (relayPins[i]!=0){
       pinMode(relayPins[i], OUTPUT);
       digitalWrite(relayPins[i], LOW); //array of relay
     }
   }
    
-   for (int i=0; i<6;i++){
+  for (int i=0; i<6;i++){
     if (itrPins[i]!=0){
-      pinMode(itrPins[i], INPUT_PULLUP);
+    pinMode(itrPins[i], INPUT_PULLUP);
     }
   }
     
   // Triggers detectMotion isr function on rising mode to turn the relay on, if the condition is met
   attachInterrupt(digitalPinToInterrupt(itrPins[4]),isr_4, CHANGE);
   attachInterrupt(digitalPinToInterrupt(itrPins[5]),isr_5,CHANGE);
-  //attachInterrupt(itrPins[5],isr_6,RISING );
-  
+
   // PWM setup for Servos
   pwm.begin();
   delay(10);
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
- 
   
-  for(int i=0; i<numItems; i++)
-    {
-     Turn_FW(i);     
-     delay(100);
-  }
-}
+  init_Turn(); //Set all Turn-Outs to Forward
+
+} // End Setup() function
 
 void loop()
 {
-String idxBlock;
+  String idxBlock;
 
-// Serial.println (digitalRead(17));
-if (idxIR!=0){
-  switch (idxIR) {
-    case 4: 
-      idxBlock= "4";
-      //Serial.println("idxIR_4= ");
-      //Serial.println(idxIR);
-      break;
-     case 5:
-      idxBlock= "5";
-      //Serial.println("idxIR_5= ");
-      //Serial.println(idxIR);
-      break; 
-  }
-   
-  valBlock = digitalRead(itrPins[idxIR]); // Read valBlockue from block sensor 
-  Serial.print("isFree prima= ");
-  Serial.println(isFree);
-    
+  // Serial.println (digitalRead(17));
+  if (idxIR!=0){
+    switch (idxIR) {
+      case 4: 
+        idxBlock= "4";
+        //Serial.println("idxIR_4= ");
+        //Serial.println(idxIR);
+        break;
+       case 5:
+        idxBlock= "5";
+        //Serial.println("idxIR_5= ");
+        //Serial.println(idxIR);
+        break; 
+    }  
+  valBlock = digitalRead(itrPins[idxIR]); // Read valBlockue from block sensor     
   if((digitalRead(itrPins[idxIR])==0)){    
     btBlock="HD"+idxBlock;
     Serial.print ("  btBlock= ");
@@ -126,7 +114,7 @@ if (idxIR!=0){
     }
     if((digitalRead(itrPins[idxIR])==1)){    
       btBlock="LD"+idxBlock;
-      Serial.println ("btBlocke= ");
+      Serial.print("btBlock= ");
       Serial.println(btBlock);
       isFree=true;
     }  
@@ -139,8 +127,7 @@ if (idxIR!=0){
       } 
       if (btBlock[0] == 'H') 
       {
-      digitalWrite(busyPins[idxIR], HIGH);
-      //Serial.print(busyPins[idxIR]);
+        digitalWrite(busyPins[idxIR], HIGH);
       }
       //Serial.println(btBlock[1]);
       btBlock="";
@@ -149,143 +136,160 @@ if (idxIR!=0){
     }
     //interrupts();
     idxIR=0;
-  }
+  } // end if idxIR!=0
   
   if (SerialBT.available())
-  
   { 
-    //Serial.println ("BT OK");
-    
-    digitalWrite(BTled, LOW); 
     for (int i=0;i<3;i++){
       servopos[i] = SerialBT.read();
-      //Serial.print(servopos[i]);
-      //Serial.println();
-     }
-     
+    } 
   SerialBT.flush();
-        if ((servopos[0] == 'l')  && (servopos[2]=='1')) { // Check Track 1
-          if (servopos[1]=='h'){
-            //Serial.println(busyPins[0]);
-            digitalWrite(relayPins[0], HIGH);
-          }
-        else{
-          digitalWrite(relayPins[0], LOW);  
-        }
+    if ((servopos[0] == 'l')  && (servopos[2]=='1')) { // Check Track 1
+      if (servopos[1]=='h'){
+        //Serial.println(busyPins[0]);
+        digitalWrite(relayPins[0], HIGH);
       }
-      if ((servopos[0] == 'l')  && (servopos[2]=='2')) { // Check Track 1
-          if (servopos[1]=='h'){
-            digitalWrite(relayPins[1], HIGH);
-          }
-        else{
-          digitalWrite(relayPins[1], LOW);  
-        }
+    else{
+      digitalWrite(relayPins[0], LOW);  
+    }
+  }
+  if ((servopos[0] == 'l')  && (servopos[2]=='2')) { // Check Track 1
+      if (servopos[1]=='h'){
+        digitalWrite(relayPins[1], HIGH);
       }
-      if ((servopos[0] == 'l')  && (servopos[2]=='3')) { // Check Track 1
-          if (servopos[1]=='h'){
-            digitalWrite(relayPins[2], HIGH);
-          }
-        else{
-          digitalWrite(relayPins[2], LOW);  
-        }
+    else{
+      digitalWrite(relayPins[1], LOW);  
+    }
+  }
+  if ((servopos[0] == 'l')  && (servopos[2]=='3')) { // Check Track 1
+      if (servopos[1]=='h'){
+        digitalWrite(relayPins[2], HIGH);
       }
-      if ((servopos[0] == 'l')  && (servopos[2]=='4')) { // Check Track 1
-          if (servopos[1]=='h'){
-            digitalWrite(relayPins[3], HIGH);
-          }
-        else{
+    else{
+      digitalWrite(relayPins[2], LOW);  
+    }
+  }
+  if ((servopos[0] == 'l')  && (servopos[2]=='4')) { // Check Track 1
+      if (servopos[1]=='h'){
+        digitalWrite(relayPins[3], HIGH);
+      }
+    else{
 
-          digitalWrite(relayPins[3], LOW);  
-        }
-      }
+      digitalWrite(relayPins[3], LOW);  
+    }
+  }
       
-    if ((servopos[0] == 'f') && (servopos[1]=='2')) {                     //if (realservo >= 1000 && realservo <1180) in case of sliders
-        Serial.println("fw23");
-        Turn_FW(2);
-      }
-    if ((servopos[0] == 'r') && (servopos[1]=='2')){
-       Serial.println("rw23");
-       Turn_BW(2);
-      }
-      
-    if ((servopos[0] == 'f') && (servopos[1]=='4')) {                     //if (realservo >= 1000 && realservo <1180) in case of sliders
-        Serial.println("fw45");
-        Turn_FW(4);
-        }
-        
-    if ((servopos[0] == 'r') && (servopos[1]=='4')){
-       Serial.println("bw45");
-       Turn_BW(4);
-      }
+  if ((servopos[0] == 'f') && (servopos[1]=='2')) {
+    Serial.println("fw23");
+    pwm.setPWM(2, 0, SERVOMIN);
+  }
+  if ((servopos[0] == 'r') && (servopos[1]=='2')){
+    Serial.println("rw23");
+    pwm.setPWM(2, 0, SERVOMAX);
+  }
     
-     if ((servopos[0] == 'f') && (servopos[1]=='5')) {                     //if (realservo >= 1000 && realservo <1180) in case of sliders
-        Serial.println("f55");
-        pwm.setPWM(10, 0, 135);
-      }
-
-     if ((servopos[0] == 'r') && (servopos[1]=='5')) {                     //if (realservo >= 1000 && realservo <1180) in case of sliders
-        Serial.println("r55");
-        pwm.setPWM(10, 0, 180);
-      }
-     
-     if ((servopos[0] == 'f') && (servopos[1]=='6')) {                     //if (realservo >= 1000 && realservo <1180) in case of sliders
-        Serial.println("f66");
-        //Turn_FW(12);
-        pwm.setPWM(12, 0, 145);
-      }
-
-     if ((servopos[0] == 'r') && (servopos[1]=='6')) {                     //if (realservo >= 1000 && realservo <1180) in case of sliders
-        Serial.println("r66");
-        //Turn_BW(12);
-        pwm.setPWM(12, 0, 185);
-      }
-     if ((servopos[0] == 'f') && (servopos[1]=='7')) {                     //if (realservo >= 1000 && realservo <1180) in case of sliders
-        Serial.println("f77");
-        Turn_FW(14);
-      }
-
-     if ((servopos[0] == 'r') && (servopos[1]=='7')) {                     //if (realservo >= 1000 && realservo <1180) in case of sliders
-        Serial.println("r77");
-        Turn_BW(14);
-      }
+  if ((servopos[0] == 'f') && (servopos[1]=='4')) { 
+    Serial.println("fw45");
+    pwm.setPWM(4, 0, SERVOMIN);
+  }
       
-    if (servopos[0] == 't') {
+  if ((servopos[0] == 'r') && (servopos[1]=='4')){
+    Serial.println("bw45");
+    pwm.setPWM(4, 0, SERVOMAX);
+  }
+    
+  if ((servopos[0] == 'f') && (servopos[1]=='5')) {
+    Serial.println("f55");
+    pwm.setPWM(5, 0, SERVOMIN);
+   }
+
+   if ((servopos[0] == 'r') && (servopos[1]=='5')) { 
+      Serial.println("r55");
+      pwm.setPWM(5, 0, SERVOMAX);
+    }
+   if ((servopos[0] == 'f') && (servopos[1]=='6')) {
+      Serial.println("f66");
+      pwm.setPWM(6, 0, SERVOMIN);
+    }
+   if ((servopos[0] == 'r') && (servopos[1]=='6')) { 
+      Serial.println("r66");
+      //Turn_BW(12);
+      pwm.setPWM(6, 0, SERVOMAX);
+   }
+   if ((servopos[0] == 'f') && (servopos[1]=='7')) { 
+      Serial.println("f77");
+      pwm.setPWM(7, 0, SERVOMIN);
+   }
+   if ((servopos[0] == 'r') && (servopos[1]=='7')) {
+      Serial.println("r77");
+      pwm.setPWM(7, 0, SERVOMAX);
+   }   
+    if ((servopos[0] == 'f') && (servopos[1]=='8')) {
+      Serial.println("f88");
+      pwm.setPWM(8, 0, SERVOMAX+10);  // Servo Muonted in Reverse Way
+    }
+   if ((servopos[0] == 'r') && (servopos[1]=='8')) { 
+      Serial.println("r88");
+      pwm.setPWM(8, 0, SERVOMIN); // Servo Muonted in Reverse Way
+   }
+   if ((servopos[0] == 'f') && (servopos[1]=='9')) { 
+      Serial.println("f99");
+      pwm.setPWM(9, 0, SERVOMAX+10);
+   }
+   if ((servopos[0] == 'r') && (servopos[1]=='9')) {
+      Serial.println("r99");
+      pwm.setPWM(9, 0, SERVOMIN);
+   }
+  if (servopos[0] == 't') {
       Serial.println("TestBT");
       SerialBT.write('k');
-    }
+  }
     
     if (servopos[0] == 'i') {
       Serial.println("reset");
-
       for (int i=0; i<6;i++){
         if (relayPins[i]!=0){
           digitalWrite(relayPins[i], LOW); //array of relay
         }
       }
-      for(int i=0; i<numItems; i++){
-       //Serial.println(i);
-       Turn_FW(i);
-      }
+      init_Turn();
     }
-  }
-//delay(100);
+  } //end if SerialBT
 }
 
 void Relay_SW (){
 Serial.print("Relay");
 }
-void Turn_BW(uint16_t servonum) {
-  //Serial.println(servonum);
-  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-    pwm.setPWM(servonum, 0, pulselen);
-    delay (5);
-  }
+
+void init_Turn(){
+  pwm.setPWM(2, 0, SERVOMIN);
+  delay(100);
+  pwm.setPWM(4, 0, SERVOMIN);
+  delay(100);   
+  pwm.setPWM(5, 0, SERVOMIN);
+  delay(100); 
+  pwm.setPWM(6, 0, SERVOMIN);
+  delay(100);
+  pwm.setPWM(7, 0, SERVOMIN);
+  delay(100);  
+  pwm.setPWM(8, 0, SERVOMAX+10); // Servo Muonted in Reverse Way
+  delay(100);
+  pwm.setPWM(9, 0, SERVOMAX+10); // Servo Muonted in Reverse Way
+  delay(100);
 }
 
-void Turn_FW(uint16_t servonum) {
- //Serial.println(servonum);
- for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
-    pwm.setPWM(servonum, 0, pulselen);
-    delay (5);
-  }
-}
+//void Turn_BW(uint16_t servonum) {
+//  //Serial.println(servonum);
+//  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+//    pwm.setPWM(servonum, 0, pulselen);
+//    delay (5);
+//  }
+//}
+//
+//void Turn_FW(uint16_t servonum) {
+// //Serial.println(servonum);
+// for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+//    pwm.setPWM(servonum, 0, pulselen);
+//    delay (5);
+//  }
+//}
